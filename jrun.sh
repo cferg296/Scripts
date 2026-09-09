27b64cb3346aa8cc8fc9eed109898e38033e411b
cferg296 <cpdf98@yahoo.com>
2026-09-08 23:33:13 -0400

    Java Script
    Will detect if java program is a gui or terminal application. Will fix
    the size of gui

---
+++ jrun.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+JAVA_PID=$$
+
+(
+    # Give Swing/AWT time to create its window
+    for _ in {1..50}; do
+        if hyprctl -i 0 clients | grep -q "pid: $JAVA_PID"; then
+
+            WINDOW="pid:$JAVA_PID"
+
+            hyprctl -i 0 dispatch "hl.dsp.window.float({
+                action = \"set\",
+                window = \"$WINDOW\"
+            })"
+
+            hyprctl -i 0 dispatch "hl.dsp.window.resize({
+                x = 1000,
+                y = 700,
+                window = \"$WINDOW\"
+            })"
+
+            hyprctl -i 0 dispatch "hl.dsp.window.center({
+                window = \"$WINDOW\"
+            })"
+
+            exit 0
+        fi
+
+        sleep 0.1
+    done
+) &
+
+exec /usr/bin/java "$@"
